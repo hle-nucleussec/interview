cf6be0cbdec3616de7a23d725828fe4bac1be9aa
Harry Le <hle@nucleussec.com>
2025-03-24 16:48:50 +1100

    Update requirements

--- src/Math/Calculator.php
+++ src/Math/Calculator.php
@@ -5,7 +5,14 @@ declare(strict_types=1);
 namespace App\Math;
 
 /**
- * Calculator class is required to support addition and division (no need for subtraction or multiplication)
+ * Design and implement a Calculator Service in PHP that supports basic arithmetic operations
+ *
+ * Because of the time restriction, the product team decides to only implement the following operations:
+ * - addition
+ * - division
+ *
+ * However, the product team wants to be able to add more operations in the future such as subtraction,
+ * multiplication and exponentiation.
  */
 class Calculator
 {
