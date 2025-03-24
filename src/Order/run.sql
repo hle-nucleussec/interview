18d296e7bd4ae19f0dbefae55c6a2ff285390d66
Harry Le <hle@nucleussec.com>
2025-03-24 17:16:38 +1100

    Add SQL test

---
+++ src/Order/run.sql
@@ -0,0 +1,21 @@
+CREATE TABLE customers
+(
+    id    INT PRIMARY KEY,
+    name  VARCHAR(100),
+    email VARCHAR(100) UNIQUE
+);
+
+CREATE TABLE orders
+(
+    id           INT PRIMARY KEY,
+    customer_id  INT,
+    total_amount DECIMAL(10, 2),
+    order_date   DATE,
+    FOREIGN KEY (customer_id) REFERENCES customers (id)
+);
+
+-- Assume the above tables are already populated with data
+
+-- Write a query to retrieve all customer names have no order or with the total order amounts less than 10
+-- sort by customer name
+
